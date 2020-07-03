9cb45e3086569cb2123e9b8635e5e49daa782596
Timothy Mickiewicz <timothy.mickiewicz@gmail.com>
2020-07-03 03:29:21 -0500

    project json file

--- src/components/content/Projects.js
+++ src/components/content/Projects.js
@@ -2,6 +2,8 @@ import React from 'react';
 import CardGroup from 'react-bootstrap/CardGroup'
 import Card from 'react-bootstrap/Card'
 import "../../style/projects.css"
+// import { info } from "../../assets/projects.json"
+// console.log(info)
 
 function Projects(props) {
   console.log(props)
