412f526dfe348f9d9baac58db4508e8ade73ddbe
Timothy Mickiewicz <timothy.mickiewicz@gmail.com>
2020-07-25 17:56:23 -0500

    skills basics are there

---
+++ src/components/Skills.tsx
@@ -0,0 +1,176 @@
+import React from 'react';
+import { IonGrid, IonRow, IonCol, IonContent, IonItem, IonLabel } from '@ionic/react';
+
+const Skills: React.FC = () => {
+  return (
+    <IonContent>
+        <IonGrid>
+            <IonRow>
+                <IonCol size-xs="12" size-md="6" size-lg="4" size-xl="2">        
+                    <IonItem color="primary">
+                        <IonLabel>
+                        JavaScript
+                        </IonLabel>
+                    </IonItem>
+                </IonCol>
+                <IonCol size-xs="12" size-md="6" size-lg="4" size-xl="2">        
+                    <IonItem color="primary">
+                        <IonLabel>
+                        HTML5
+                        </IonLabel>
+                    </IonItem>
+                </IonCol>
+                <IonCol size-xs="12" size-md="6" size-lg="4" size-xl="2">        
+                    <IonItem color="primary">
+                        <IonLabel>
+                        CSS3
+                        </IonLabel>
+                    </IonItem>
+                </IonCol>
+                <IonCol size-xs="12" size-md="6" size-lg="4" size-xl="2">        
+                    <IonItem color="primary">
+                        <IonLabel>
+                        Node.js
+                        </IonLabel>
+                    </IonItem>
+                </IonCol>
+                <IonCol size-xs="12" size-md="6" size-lg="4" size-xl="2">        
+                    <IonItem color="primary">
+                        <IonLabel>
+                        CoffeeScript
+                        </IonLabel>
+                    </IonItem>
+                </IonCol>
+                <IonCol size-xs="12" size-md="6" size-lg="4" size-xl="2">        
+                    <IonItem color="primary">
+                        <IonLabel>
+                        MySQL
+                        </IonLabel>
+                    </IonItem>
+                </IonCol>
+                <IonCol size-xs="12" size-md="6" size-lg="4" size-xl="2">        
+                    <IonItem color="primary">
+                        <IonLabel>
+                        MongoDB
+                        </IonLabel>
+                    </IonItem>
+                </IonCol>
+                <IonCol size-xs="12" size-md="6" size-lg="4" size-xl="2">        
+                    <IonItem color="danger">
+                        <IonLabel>
+                        React
+                        </IonLabel>
+                    </IonItem>
+                </IonCol>
+                <IonCol size-xs="12" size-md="6" size-lg="4" size-xl="2">        
+                    <IonItem color="danger">
+                        <IonLabel>
+                        Ionic
+                        </IonLabel>
+                    </IonItem>
+                </IonCol>
+                <IonCol size-xs="12" size-md="6" size-lg="4" size-xl="2">        
+                    <IonItem color="success">
+                        <IonLabel>
+                        jQuery
+                        </IonLabel>
+                    </IonItem>
+                </IonCol>
+                <IonCol size-xs="12" size-md="6" size-lg="4" size-xl="2">        
+                    <IonItem color="success">
+                        <IonLabel>
+                        Material UI
+                        </IonLabel>
+                    </IonItem>
+                </IonCol>
+                <IonCol size-xs="12" size-md="6" size-lg="4" size-xl="2">        
+                    <IonItem color="success">
+                        <IonLabel>
+                        BootStrap
+                        </IonLabel>
+                    </IonItem>
+                </IonCol>
+                <IonCol size-xs="12" size-md="6" size-lg="4" size-xl="2">        
+                    <IonItem color="success">
+                        <IonLabel>
+                        Sequelize
+                        </IonLabel>
+                    </IonItem>
+                </IonCol>
+                <IonCol size-xs="12" size-md="6" size-lg="4" size-xl="2">        
+                    <IonItem color="success">
+                        <IonLabel>
+                        Mongoose
+                        </IonLabel>
+                    </IonItem>
+                </IonCol>
+                <IonCol size-xs="12" size-md="6" size-lg="4" size-xl="2">        
+                    <IonItem color="warning">
+                        <IonLabel>
+                        Moment.js
+                        </IonLabel>
+                    </IonItem>
+                </IonCol>
+                <IonCol size-xs="12" size-md="6" size-lg="4" size-xl="2">        
+                    <IonItem color="warning">
+                        <IonLabel>
+                        Passport.js
+                        </IonLabel>
+                    </IonItem>
+                </IonCol>
+                <IonCol size-xs="12" size-md="6" size-lg="4" size-xl="2">        
+                    <IonItem color="warning">
+                        <IonLabel>
+                        Bcrypt
+                        </IonLabel>
+                    </IonItem>
+                </IonCol>
+                <IonCol size-xs="12" size-md="6" size-lg="4" size-xl="2">        
+                    <IonItem color="warning">
+                        <IonLabel>
+                        Inquirer
+                        </IonLabel>
+                    </IonItem>
+                </IonCol>
+                <IonCol size-xs="12" size-md="6" size-lg="4" size-xl="2">        
+                    <IonItem color="warning">
+                        <IonLabel>
+                        Express.js
+                        </IonLabel>
+                    </IonItem>
+                </IonCol>
+                <IonCol size-xs="12" size-md="6" size-lg="4" size-xl="2">        
+                    <IonItem color="warning">
+                        <IonLabel>
+                        Validator
+                        </IonLabel>
+                    </IonItem>
+                </IonCol>
+                <IonCol size-xs="12" size-md="6" size-lg="4" size-xl="2">        
+                    <IonItem color="warning">
+                        <IonLabel>
+                        Axios
+                        </IonLabel>
+                    </IonItem>
+                </IonCol>
+                <IonCol size-xs="12" size-md="6" size-lg="4" size-xl="2">        
+                    <IonItem color="warning">
+                        <IonLabel>
+                        React-Charts
+                        </IonLabel>
+                    </IonItem>
+                </IonCol>
+                <IonCol size-xs="12" size-md="6" size-lg="4" size-xl="2">        
+                    <IonItem color="warning">
+                        <IonLabel>
+                        EsLint
+                        </IonLabel>
+                    </IonItem>
+                </IonCol>
+            </IonRow>
+        </IonGrid>
+    </IonContent>
+  )
+}
+
+export default Skills
--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -3,19 +3,13 @@ import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/rea
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab3.css';
 import Header from '../components/Header'
+import Skills from "../components/Skills"
 
 const Tab3: React.FC = () => {
   return (
     <IonPage>
       <Header />
-      <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
-      </IonContent>
+      <Skills />
     </IonPage>
   );
 };
